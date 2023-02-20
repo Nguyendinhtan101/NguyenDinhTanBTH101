f32a93a24feb059cb7034ca8ee2194c1e617122d
Your Name <you@example.com>
2023-02-20 16:20:13 +0700

    Ngay 20/02

---
+++ NguyenDinhTanBTH/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenDinhTanBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
